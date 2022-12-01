013a4a80be0ab2af2fbd08f7c165f9fa8498d796
Sumiyabazar Gerelt Od <sumiyabaza.g@gso.schule.koeln>
2022-12-01 12:05:30 +0100

    Aufgabe 1 Fertig

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double y;
+double m;
+double l;
+double BMI;
+Console.Write("how tall are you ? (cm):");
+y= Convert.ToDouble(Console.ReadLine());
+Console.Write("Geben Sie ihre Gewicht?");
+m= Convert.ToDouble(Console.ReadLine());
+l = y / 100;
+BMI = m / (l * l);
+Console.WriteLine(BMI);
+if (BMI < 18.5)
+{
+    Console.WriteLine("Untergewicht");
+}
+else if  (BMI > 25)
+{
+    Console.WriteLine("Ubergewicht");
+
+}
+else
+{
+    Console.WriteLine("Medium");
+}
